4810f7621fb5db1df243dc0f701f8170efc4b8bd
Rajkumar <rajshine55@gmail.com>
2018-07-06 18:54:26 +0530

    models added

---
+++ school_management_system/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+
+class School(models.Model):
+    name = models.CharField(max_length=100, null=False, blank=False)
+    city = models.CharField(max_length=50, null=False, blank=False)
+    pin_code = models.CharField(max_length=6, null=False, blank=False)
+    phone_number = models.CharField(max_length=20, null=False, blank=False, help_text='Phone number with STD code')
+    email = models.EmailField(max_length=50)
+    website = models.URLField(help_text='Link to the website')
+
+    class Meta:
+        db_table = 'school'
+
+
+class Student(models.Model):
+    student_name = models.CharField(max_length=100, null=False, blank=False)
+    admission_number = models.IntegerField(null=False, blank=False)
+    address = models.TextField(null=False, blank=False)
+    school = models.ForeignKey(School, related_name='school_id', on_delete=models.CASCADE, null=False, blank=False)
+    class_number = models.IntegerField(null=False, blank=False, help_text='In which class the student is enrolled')
+    date_of_joining = models.DateField(auto_now_add=True)
+    emergency_contact_number = models.CharField(max_length=20, null=False, blank=False)
+
+    class Meta:
+        db_table = 'student'
